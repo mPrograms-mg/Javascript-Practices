f1bf5c9529928b36be192f4a0609257a0dd2c650
mPrograms-mg <maheshkshirsagar128@gmail.com>
2023-08-22 23:40:36 +0530

    Add Array snippet

---
+++ Array/index.js
@@ -0,0 +1,26 @@
+// console.log("Hello Array");
+
+let a = [1,2,,3,4,5] // Number Array
+
+let b = ['a','b','c'] // String Array
+
+let c = [true,false] // boolean Array
+
+let MixArray = [1,2.5,"Mahesh",true]; // Mix Array
+
+console.log(MixArray);
+
+//Create New Array with new Array method
+
+const newArray = new Array("a","b",2,false);
+
+console.log(newArray);
+
+// Array Accessing
+
+//start index with 0
+
+/*
+* given Array [11,22,33,44,55]
+* index/pos -> [0,1,2,3,4] // last element index = arr.length-2;
+*/
