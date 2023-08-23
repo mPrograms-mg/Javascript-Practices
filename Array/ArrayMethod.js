96f6e4b5402041aaafa3db9c587730cd34ae0d5c
mPrograms-mg <maheshkshirsagar128@gmail.com>
2023-08-23 23:58:43 +0530

    add array join, concat method

--- Array/ArrayMethod.js
+++ Array/ArrayMethod.js
@@ -10,6 +10,10 @@ console.log(arr);
 arr.pop();
 console.log(arr); 
 
+// Also return value first element
+let popElement = arr.pop()
+console.log(popElement);
+
 //Add element in starting index
 arr.unshift(true,2.55,"Mahesh")
 console.log(arr);
@@ -17,3 +21,20 @@ console.log(arr);
 // remove element in starting index
 arr.shift();
 console.log(arr);
+
+// Also return value first element
+let element = arr.shift()
+console.log(element);
+
+// Concat the Array Element
+
+let a = [1,2,3,4,5];
+let b = ["Mahesh","Kshirsagar",22.5,true,2.1101,false]
+let c = a.concat(b);
+console.table(c);
+
+// convent array method in string or add any symbol to the element use join
+
+// let joinArr = c.join("-");
+let joinArr = c.join(", ");
+console.log(joinArr);
