ad0fa41986848eda0d127791d025202f6c9d0c97
mPrograms-mg <maheshkshirsagar128@gmail.com>
2023-08-23 23:50:58 +0530

    commit

--- Array/ArrayMethod.js
+++ Array/ArrayMethod.js
@@ -2,10 +2,18 @@
 let arr = [1,2,3,4,5];
 
 
-// Add element in last
+// Add element at last index
 arr.push("Mahesh");
 console.log(arr);
 
-// Remove element in last
+// Remove element at last index
 arr.pop();
 console.log(arr); 
+
+//Add element in starting index
+arr.unshift(true,2.55,"Mahesh")
+console.log(arr);
+
+// remove element in starting index
+arr.shift();
+console.log(arr);
