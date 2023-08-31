9ed1b807ab3a3f78ba461cceb26b484f198858ff
mPrograms-mg <maheshkshirsagar128@gmail.com>
2023-08-31 23:43:25 +0530

    add new function

--- Function/index.js
+++ Function/index.js
@@ -15,3 +15,17 @@ const arrFun = (val) =>{
 
 console.log(arrFun());
 console.log(arrFun("Mahesh"));
+
+// ultimate parameter of function
+
+function ultimatePara(){
+    let sum = 0;
+    for(var i=0; i<arguments.length; i++){
+        sum += arguments[i];
+    }
+    return sum;
+
+}
+
+let result = ultimatePara(1,2,3,4,5);
+console.log(result);
