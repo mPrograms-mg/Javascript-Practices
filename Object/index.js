b79a343c79d41e6047220c91958fe57b5da7d820
mPrograms-mg <maheshkshirsagar128@gmail.com>
2023-09-04 22:56:52 +0530

    Add object

---
+++ Object/index.js
@@ -0,0 +1,14 @@
+const Obj  = {
+    name : "Mahesh Kshirsagar Object",
+    age: 22,
+    city:"Surat",
+    Pin:395012,
+    Marriaged:false,
+}
+
+console.log(Obj)
+
+console.log(Obj.name);
+console.log(Obj.Pin);
+console.log(Obj.city);
+
