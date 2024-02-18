447db1f0adfb69c0a72cf922b049ee5f926c6e51
Mahesh K <maheshkshirsagar128@gmail.com>
2024-02-18 23:53:40 +0530

    String Reverse

---
+++ HackerRank-10Days-JS/Day9.js
@@ -0,0 +1,15 @@
+// Reverse String
+
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+function reverseString(s) {
+  if (str) {
+    let str = s.split("").reverse();
+    return str.join("");
+  }
+}
+
+reverseString("123");
+
+console.log(reverseString("123"));
