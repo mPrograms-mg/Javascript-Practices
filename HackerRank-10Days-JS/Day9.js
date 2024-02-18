0c0a5ec5f7512d58d3c3b1aa1b66831ce8dc88d6
Mahesh K <maheshkshirsagar128@gmail.com>
2024-02-19 00:03:40 +0530

    reverse string update

--- HackerRank-10Days-JS/Day9.js
+++ HackerRank-10Days-JS/Day9.js
@@ -13,3 +13,50 @@ function reverseString(s) {
 reverseString("123");
 
 console.log(reverseString("123"));
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+     try {
+         s = [...s].reverse().join('');
+    } catch (e) {
+         console.log('s.split is not a function');
+    }
+
+    console.log(s);
+}
+
+
+
+function main() {
+    const s = eval(readLine());
+    
+    reverseString(s);
+}
