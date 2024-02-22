4315d6e18791f90214618e52eea597488728e686
Mahesh K <maheshkshirsagar128@gmail.com>
2024-02-22 07:12:17 +0530

    Day12 project solved

--- HackerRank-10Days-JS/Day12.js
+++ HackerRank-10Days-JS/Day12.js
@@ -1,26 +1,4 @@
-'use strict';
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
 
 /*
  * Return a count of the total number of objects 'o' satisfying o.x == o.y.
@@ -28,20 +6,24 @@ function readLine() {
  * Parameter(s):
  * objects: an array of objects with integer properties 'x' and 'y'
  */
+
+
+
+let totalCount = 0;
 function getCount(objects) {
-    
+      for (let obj in objects) {
+      if (objects[obj].x === objects[obj].y) {
+        totalCount++;
+      }
+  }
+  return totalCount;
 }
 
-
-function main() {
-    const n = +(readLine());
-    let objects = [];
-    
-    for (let i = 0; i < n; i++) {
-        const [a, b] = readLine().split(' ');
-        
-        objects.push({x: +(a), y: +(b)});
-    }
-    
-    console.log(getCount(objects));
-}
+let arrayNew = [
+    { x: 1, y: 1 },
+    { x: 2, y: 3 },
+    { x: 3, y: 3 },
+    { x: 3, y: 4 },
+    { x: 4, y: 5 }
+  ]
+console.log("Count Obj....",getCount(arrayNew));
