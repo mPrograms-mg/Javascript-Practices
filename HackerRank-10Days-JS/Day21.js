28c0a9d03890eed3c350c94c76f77dddca7fba24
Mahesh K <maheshkshirsagar128@gmail.com>
2024-03-04 23:40:34 +0530

    day21

---
+++ HackerRank-10Days-JS/Day21.js
@@ -0,0 +1,14 @@
+
+// The function must return a RegExp object that matches every integer in some string .
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+  var re =   /\d+/g;
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
