de178deb8907ae2f5872638eb67df0032151185b
Mahesh K <maheshkshirsagar128@gmail.com>
2024-04-06 23:21:27 +0530

    three sum problem add

--- Problem/TwoSum.js
+++ Problem/TwoSum.js
@@ -38,17 +38,52 @@ function twoSum2(nums, target) {
 
 //method2 retun target value sum index
 
-function twoSum3(nums, target) {
+// function twoSum3(nums, target) {
+//   let indexs = [];
+//   for (let index = 0; index < nums.length; index++) {
+//     for (let j = index+1; j < nums.length; j++) {
+//       if (nums[index] + nums[j] === target) {
+//         indexs.push(index)
+//         indexs.push(j)
+//       }
+//     }
+//   }
+//   return indexs;
+// }
+
+// console.log(twoSum3([2,7,11,15],9));
+
+function twoSum4(nums, target) {
   let indexs = [];
-  for (let index = 0; index < nums.length; index++) {
-    for (let j = index+1; j < nums.length; j++) {
-      if (nums[index] + nums[j] === target) {
-        indexs.push(index)
-        indexs.push(j)
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = i + 2; k < nums.length; k++) {
+        if (nums[i] + nums[j] + nums[k] === target) {
+          indexs.push(index);
+          indexs.push(j);
+          indexs.push(k);
+        }
       }
     }
   }
   return indexs;
 }
 
-console.log(twoSum3([2,7,11,15],9));
+function threeSum(nums, target) {
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (nums[i] + nums[j] + nums[k] === target) {
+          // result.push([nums[i], nums[j], nums[k]]);
+          result.push(i);
+          result.push(j);
+          result.push(k);
+        }
+      }
+    }
+  }
+  return result;
+}
+
+console.log(threeSum([10, 8, 2, 5, 6], 20));
