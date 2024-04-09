27d05cdc7b708a87f32d6fbd50e957796684c8bf
Mahesh K <maheshkshirsagar128@gmail.com>
2024-04-10 01:24:53 +0530

    add object literals in object programs

--- Object/index.js
+++ Object/index.js
@@ -1,51 +1,63 @@
-const Obj  = {
-    name : "Mahesh Kshirsagar Object",
-    age: 22,
-    city:"Surat",
-    Pin:395012,
-    Marriaged:false,
-}
+//Object is collection of key value pairs
 
-console.log(Obj)
+const Obj = {
+  name: "Mahesh Kshirsagar Object",
+  age: 22,
+  city: "Surat",
+  Pin: 395012,
+  Marriaged: false,
+};
+
+console.log(Obj);
 console.log(Obj.name);
 console.log(Obj.Pin);
 console.log(Obj.city);
 
-
 // create Object using new method
-const Objs  = new Object();
-Objs.name = 'Mahesh';
+const Objs = new Object();
+Objs.name = "Mahesh";
 Objs.age = 23;
-Objs.add = 'Surat'
+Objs.add = "Surat";
 console.log(Objs);
 
-
 // create Object using fuctions
-function ObjCreation(id,num,value){
-      this.id = id;
-      this.number = num;
-      this.value = value;
+function ObjCreation(id, num, value) {
+  this.id = id;
+  this.number = num;
+  this.value = value;
 }
 
-const newObj = new ObjCreation(1,123,'Mahesh')
-console.log(newObj)
-
+const newObj = new ObjCreation(1, 123, "Mahesh");
+console.log(newObj);
 
 // access object value
-console.log(newObj.id)
-console.log(newObj['number'])
+console.log(newObj.id);
+console.log(newObj["number"]);
 
 // assign new value to object
-newObj['number'] = 4342;
-newObj.add= 'Surat';
-
+newObj["number"] = 4342;
+newObj.add = "Surat";
 
 // delete object key
-delete newObj.add
-console.log(newObj)
+delete newObj.add;
+console.log(newObj);
 
+//Object Literals
 
-
-// Object Methods
-
-
+let obj = {
+  name: "Mahesh Obj",
+  age: 18,
+  greet() {
+    return "Good Morning, Mahesh";
+  },
+  age(num) {
+    return `Mahesh is a ${num} year old`;
+  },
+  package(salary, totalMonth) {
+    return `Mahesh Earning ${salary * totalMonth} per Annum`;
+  },
+};
+console.log(obj);
+console.log(obj.greet());
+console.log(obj.age(23));
+console.log(obj.package(25000, 12));
