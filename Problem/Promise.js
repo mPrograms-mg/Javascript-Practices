2bcdf432c11e499290c7f596d4a29877d265a254
Mahesh K <maheshkshirsagar128@gmail.com>
2024-04-12 18:43:31 +0530

    Promise program

---
+++ Problem/Promise.js
@@ -0,0 +1,38 @@
+// Asynchronous Task : Allow multiple Operations to be Performed concurrently without waiting.
+// Does't block the excutions flow and allows ther promgrams to continue
+// EX (I/O Operations, network request, fetching data, Handle with Callbacks, Promises, Async/Await)
+
+// Promise with Resolve state
+// const myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Promise Excuted....");
+//   }, 2000);
+// });
+
+// myPromise.then((value) => {
+//   console.log("Promise Resolved After");
+//   console.log(`This is Promise Value ${value}`);
+// });
+
+// Why do you need a promise
+// Promises are used to handle asynchronous operations.
+// They provide an alternative approach for callbacks by reducing the callback hell and writing the cleaner code.
+
+// What are the three states of promise
+// Promises have three states:
+// Pending: This is an initial state of the Promise before an operation begins
+// Fulfilled: This state indicates that the specified operation was completed.
+// Rejected: This state indicates that the operation did not complete. In this case an error value will be thrown
+
+const promise = new Promise((resolve, reject) => {
+  let isTrue = false;
+  if (isTrue) {
+    resolve("I'm a Promise!");
+  } else {
+    reject("Error in Promise");
+  }
+});
+
+promise
+  .then((value) => console.log(value))
+  .catch((error) => console.log(error));
