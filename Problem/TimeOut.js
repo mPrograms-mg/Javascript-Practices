8678b18ad2cdd446ceba0257c553dc1075dc07f6
Mahesh K <maheshkshirsagar128@gmail.com>
2024-04-13 23:31:06 +0530

    Common Time out problem

---
+++ Problem/TimeOut.js
@@ -0,0 +1,7 @@
+//setTimeout() = function in Javacript that allows you to Schedule the executions of a functions after an amount of time (milliseconds)
+
+const clearTime = setTimeout(() => {
+  console.log("This is setTimeOut");
+}, 2000);
+
+clearTimeout(clearTime);
