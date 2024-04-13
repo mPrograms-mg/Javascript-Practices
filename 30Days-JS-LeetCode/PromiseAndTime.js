039bc383919185e5416020fcb5c1ca7d4db5780c
Mahesh K <maheshkshirsagar128@gmail.com>
2024-04-13 23:20:13 +0530

    add Promise and Time in 30 days js leetcode

--- 30Days-JS-LeetCode/PromiseAndTime.js
+++ 30Days-JS-LeetCode/PromiseAndTime.js
@@ -14,3 +14,39 @@ const promise2 = new Promise((resolve) => setTimeout(() => resolve(5), 60));
 
 addTwoPromises(promise1, promise2).then(console.log); // 4
 // console.log(addTwoPromises);
+
+var cancellable = function (fn, args, t) {
+  const cancleFn = () => {
+    clearTimeout(timer);
+  };
+
+  const timer = setTimeout(() => {
+    fn(...args);
+  }, t);
+
+  return cancleFn;
+};
+
+const result = [];
+
+const fn = (x) => x * 5;
+const args = [2],
+  t = 20,
+  cancelTimeMs = 50;
+
+const start = performance.now();
+
+const log = (...argsArr) => {
+  const diff = Math.floor(performance.now() - start);
+  result.push({ time: diff, returned: fn(...argsArr) });
+};
+
+const cancel = cancellable(log, args, t);
+
+const maxT = Math.max(t, cancelTimeMs);
+
+setTimeout(cancel, cancelTimeMs);
+
+setTimeout(() => {
+  console.log(result); // [{"time":20,"returned":10}]
+}, maxT + 15);
