6aad144c3c5847c7538432cad5e1b4d3e1b4ae58
Mahesh K <maheshkshirsagar128@gmail.com>
2024-04-16 23:51:11 +0530

    excute asysnchronous fuctions

--- 30Days-JS-LeetCode/PromiseAndTime.js
+++ 30Days-JS-LeetCode/PromiseAndTime.js
@@ -148,3 +148,25 @@ const logDebouce = debounce(console.log, 1000);
 logDebouce("Hello");
 logDebouce("Hello");
 logDebouce("Hello");
+
+// Execute Asynchronous Functions in Parallel
+
+var promiseAll = function (functions) {
+  return new Promise((resolve, reject) => {
+    let res = Array(functions.length);
+    let waitingFor = functions.length;
+
+    for (let i = 0; i < functions.length; i++) {
+      functions[i]()
+        .then((result) => {
+          res[i] = result;
+          waitingFor--;
+          if (waitingFor === 0) resolve(res);
+        })
+        .catch(reject);
+    }
+  });
+};
+
+const promise = promiseAll([() => new Promise((res) => res(42))]);
+promise.then(console.log); // [42]
