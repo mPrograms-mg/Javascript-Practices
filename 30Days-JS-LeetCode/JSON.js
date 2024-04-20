f3beada722a597d69dbcfb7037274d0c0fb6f036
Mahesh K <maheshkshirsagar128@gmail.com>
2024-04-20 23:11:39 +0530

    30 days leetcode js JSON

--- 30Days-JS-LeetCode/JSON.js
+++ 30Days-JS-LeetCode/JSON.js
@@ -31,3 +31,19 @@ let arr = [1, 2, 3, 4, 5],
   size = 3;
 
 console.log(chunk(arr, size));
+
+// Array Prototype Last
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+
+Array.prototype.last = function () {
+  if (this.length === 0) {
+    return -1;
+  }
+  return this[this.length - 1];
+};
+
+const Arr = [1, 2, 3];
+console.log(Arr.last());
+
+const Arr2 = [];
+console.log(Arr2.last());
