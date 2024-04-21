27934fbd1b766ad4d06e8ac17571bc700e675c33
Mahesh K <maheshkshirsagar128@gmail.com>
2024-04-21 22:56:11 +0530

    30 days leetcode JS JSON

--- 30Days-JS-LeetCode/JSON.js
+++ 30Days-JS-LeetCode/JSON.js
@@ -47,3 +47,25 @@ console.log(Arr.last());
 
 const Arr2 = [];
 console.log(Arr2.last());
+
+// Group By
+// Write code that enhances all arrays such that you can call the array.groupBy(fn) method on any array and it will return a grouped version of the array.
+
+Array.prototype.groupBy = function (fn) {
+  return this.reduce((grouped, item) => {
+    const key = fn(item);
+
+    if (!grouped[key]) {
+      grouped[key] = [];
+    }
+
+    grouped[key].push(item);
+
+    return grouped;
+  }, {});
+};
+
+const fn = function (item) {
+  return String(item > 2);
+};
+console.log([1, 2, 3].groupBy(fn));
