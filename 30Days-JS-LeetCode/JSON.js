152c6bd20a4f3ff7aa6479eb6cb844cf2b7aa286
Mahesh K <maheshkshirsagar128@gmail.com>
2024-04-23 19:52:47 +0530

    30 day js JSON flat array

--- 30Days-JS-LeetCode/JSON.js
+++ 30Days-JS-LeetCode/JSON.js
@@ -117,3 +117,28 @@ let arr1 = [
   ];
 
 console.log(join(arr1, arr2));
+
+// Flatten Deeply Nested Array using Recursion Technique
+const flat = function (arr, n) {
+  if (n === 0) {
+    return arr.slice();
+  }
+
+  const flatArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      const nestedArray = flat(arr[i], n - 1);
+      flatArray.push(...nestedArray);
+    } else {
+      flatArray.push(arr[i]);
+    }
+  }
+
+  return flatArray;
+};
+
+arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
+n = 2;
+
+console.log(flat(arr, n));
