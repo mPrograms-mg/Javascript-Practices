91ba3a23a6dace50de828f3990ffb67ff26e13a6
Mahesh K <maheshkshirsagar128@gmail.com>
2024-06-03 11:01:05 +0530

    palindrome num check method 2

--- Problem/PalindromeNumber.js
+++ Problem/PalindromeNumber.js
@@ -10,3 +10,19 @@ function palindromeNum(n){
 
 console.log("FunctionCall....",palindromeNum(1232));
 
+
+// Method 2
+
+// function PalidromeNum(num) {
+//     let rev = 0
+//     let temp = num
+//     let rem;
+//     while (num > 0) {
+//       rem = num % 10;
+//       rev = rev * 10 + rem;
+//       num = Math.floor(num / 10);
+//     }
+//     return  rev === temp ? console.log(`True...${temp} is Palindrome`) : console.log(`false...${temp} is not palindrome`)
+//   }
+  
+//   PalidromeNum(3241);
