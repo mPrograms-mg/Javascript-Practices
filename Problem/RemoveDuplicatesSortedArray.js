441f74a0dd75b4b6031c195c0c8131b29730a52d
Mahesh K <maheshkshirsagar128@gmail.com>
2024-06-07 00:15:01 +0530

    Remove Duplicates from Sorted Array method 2

--- Problem/RemoveDuplicatesSortedArray.js
+++ Problem/RemoveDuplicatesSortedArray.js
@@ -1,10 +1,28 @@
 // Remove Duplicates from Sorted Array
 
+// Method 1
+// var removeDuplicates = function (nums) {
+//   const sortArr = nums.sort();
+//   const uniqElement = [...new Set(sortArr)];
+//   console.log(uniqElement);
+// };
+
+// nums = [1, 1, 2];
+// removeDuplicates(nums);
+
 var removeDuplicates = function (nums) {
-  const sortArr = nums.sort();
-  const uniqElement = [...new Set(sortArr)];
-  console.log(uniqElement);
+  for (let index = 0; index < nums.length; index++) {
+    for (let j = index + 1; j < nums.length; j++) {
+      if (nums[index] === nums[j]) {
+        nums.splice(index, 1);
+      }
+    }
+  }
+
+  return nums;
 };
 
-nums = [1, 1, 2];
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 removeDuplicates(nums);
+
+console.log(removeDuplicates(nums));
