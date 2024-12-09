9ce1d6e7871e58ce6fbeab2a13bc11a08900ba00
Mahesh K <maheshkshirsagar128@gmail.com>
2024-12-09 21:39:34 +0530

    code update

--- Problem/findlongWord.js
+++ Problem/findlongWord.js
@@ -1,17 +1,52 @@
+// Method-1
 // Program to find longest word in a given sentence!!
 
 const longWord =
   "I am Mahesh Mohan Kshirsagar, I am Software Engineer having 2 year experiance";
 
-findlongWord(longWord);
+// findlongWord(longWord);
 
-function findlongWord(word) {
-  const longWord = word.split(" ");
-  let wordLong = 0;
-  for (const element of longWord) {
-    if (element.length > wordLong) {
-      wordLong = element.length;
+// function findlongWord(word) {
+//   const longWord = word.split(" ");
+//   let wordLong = 0;
+//   for (const element of longWord) {
+//     if (element.length > wordLong) {
+//       wordLong = element.length;
+//     }
+//   }
+//   console.log("longest word in a sentence", wordLong);
+// }
+
+// function findLong(word) {
+//   const strSplit = word.split(' ')
+//   const longWord = 0;
+//   console.log("Word...", strSplit);
+//   for (let i = 0; i < strSplit.length; i++) {
+//     console.log(`${strSplit[i]}`);
+//     if (strSplit[i].length > longWord) {
+//       longWord = strSplit[i].length
+//     }
+//   }
+
+//   return longWord;
+// }
+
+// findLong(longWord)
+
+
+function longestWordLen(longWord) {
+  let n = s.length
+  let res = 0
+  let curreLen = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (s[i] != '') {
+
+    } else {
+      res = Math.max()
     }
+
   }
-  console.log("longest word in a sentence", wordLong);
 }
+
+console.log(longestWordLen(s));
