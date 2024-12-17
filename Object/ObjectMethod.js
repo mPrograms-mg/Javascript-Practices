b2d7ff6825683c7484522948943f6dca6f290df2
Mahesh K <maheshkshirsagar128@gmail.com>
2024-12-17 13:55:02 +0530

    method changes

--- Object/ObjectMethod.js
+++ Object/ObjectMethod.js
@@ -1,35 +1,36 @@
 let personObj = {
-    id:101,
-    name:'Mahesh Kshirsagar',
-    add:'Surat',
-    num:'9234452123',
-    age:23,
+  id: 101,
+  name: "Mahesh Kshirsagar",
+  add: "Surat",
+  num: "9234452123",
+  age: 23,
+};
 
-}
-
+const perObj = { ...personObj };
+perObj.name = "Sanjay";
+console.log(personObj.name);
 
 // fetch Object keys
-let keys = Object.keys(personObj)
-console.log(keys);
+let keys = Object.keys(personObj);
+console.log("key...", keys);
 
 //fetch Object values
-let values = Object.values(personObj)
-console.log(values);
+let values = Object.values(personObj);
+console.log("Values...", values);
 
 // return the array of given Object key value pair
-let objEnteries = Object.entries(personObj)
-console.log(objEnteries);
-
+let objEnteries = Object.entries(personObj);
+console.log("Enteries..", objEnteries);
 
 // Object freeze use to restrict add or update & delete object
 // Object.freeze(personObj)
-// console.log(personObj.id); 
+// console.log(personObj.id);
 
 // Object seal use to allow  update object
-Object.seal(personObj)
-personObj.id = 122
-console.log(personObj)
+Object.seal(personObj);
+personObj.id = 122;
+console.log("Seal...", personObj);
 
 // Assingn object to new variable
-let assignObj = Object.assign({edu:'BE'},personObj)
-console.log(assignObj);
+let assignObj = Object.assign({ edu: "BE" }, personObj);
+console.log("Assing..", assignObj);
