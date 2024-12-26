1f21af2a6679b0cd569e5c43042ab1228197cf1e
Mahesh K <maheshkshirsagar128@gmail.com>
2024-12-26 23:42:36 +0530

    Leet code pluse method

---
+++ Problem/LeetCodePlusOne.js
@@ -0,0 +1,11 @@
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+let digits = [9];
+let ans = Number(digits.join(""));
+let res = (ans + 1).toString().split("");
+let result = res.map(Number);
+console.log(result);
