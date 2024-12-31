3fb0c756dfc3a6be5f8d8c4768e90a8696d66905
Mahesh K <maheshkshirsagar128@gmail.com>
2024-12-31 22:45:01 +0530

    add new methods

--- Problem/largeEleInNestArray.js
+++ Problem/largeEleInNestArray.js
@@ -7,3 +7,14 @@ console.log(newArr);
 const maxValue = Math.max(...newArr);
 
 console.log(maxValue);
+
+function findLargestElement(arr) {
+  return arr.reduce((max, item) => {
+    if (Array.isArray(item)) {
+      return Math.max(max, findLargestElement(item)); // Recurse for nested arrays
+    }
+    return Math.max(max, item); // Compare numbers
+  }, -Infinity);
+}
+
+console.log(findLargestElement(nestedArray));
