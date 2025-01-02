359ae092b9c871e26c4298cdb623b2a05fe50b0c
Mahesh K <maheshkshirsagar128@gmail.com>
2025-01-02 23:03:52 +0530

    strings occurrence count

---
+++ Problem/StringOccurrence.js
@@ -0,0 +1,23 @@
+// 16. Given a string, write a javascript function to count the occurrences of each character in the string.
+
+console.log("Hello...");
+const strings = "Mahesh";
+const str = "h";
+let count = 0;
+for (let i = 0; i < strings.length; i++) {
+  if (strings.charAt(i) === str) {
+    count += 1;
+  }
+}
+
+console.log(count);
+
+let myString =
+  "John Doe has 5 oranges while Jane Doe has only 2 oranges, Jane gave Mike 1 of her orange so she is now left with only 1 Orange.";
+let mySubString = "ORANGE";
+
+let myStringLC = myString.toLowerCase();
+let mySubStringLC = mySubString.toLowerCase();
+
+console.log(myStringLC.split(mySubStringLC));
+let counts = myStringLC.split(mySubStringLC).length - 1;
