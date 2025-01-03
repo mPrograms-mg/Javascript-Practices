9057129014708a229a0dc033676c27ff452f00cc
Mahesh K <maheshkshirsagar128@gmail.com>
2025-01-03 21:17:10 +0530

    javascript sort array functions

---
+++ Problem/SortArrayAscending.js
@@ -0,0 +1,5 @@
+// Write a javascript function that sorts an array of numbers in ascending order.
+
+const array = [34, 54, 67, 12, 44];
+const sortArray = array.sort((a, b) => a - b);
+console.log(sortArray);
