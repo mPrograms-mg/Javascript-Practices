3b5e0cd9549265a87448a8f0101cc0eea149234c
Mahesh K <maheshkshirsagar128@gmail.com>
2025-01-04 22:06:13 +0530

    reverse word with different method

--- Problem/reverseWord.js
+++ Problem/reverseWord.js
@@ -1,13 +1,14 @@
 // Write a javascript function that reverses the order of words in a sentence without using the built-in reverse() method.
 
-const string = "Mahesh";
+const string = "Mahesh Mohan Kshirsagar";
 
-// for (const element of string) {
-//   console.log(element);
+// let reverStr = "";
+// for (let index = string.length; index >= 0; index--) {
+//   const element = string[index];
+//   reverStr += element;
 // }
-let reverStr = "";
-for (let index = string.length; index >= 0; index--) {
-  const element = string[index];
-  reverStr += element;
-}
-console.log(reverStr);
+// console.log(reverStr);
+
+const reverArr = string.split(" ");
+reverArr.reverse().join("");
+console.log(reverArr);
