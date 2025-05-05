bc475b4486064c59154bcb2cdf3b1080f493a9ef
Mahesh K <maheshkshirsagar128@gmail.com>
2025-05-05 21:57:00 +0530

    curry with return function

--- Problem/Curry.js
+++ Problem/Curry.js
@@ -9,3 +9,25 @@ function sum(a) {
 }
 
 console.log(sum(1)(2)(3));
+
+// Curry with function return
+
+function multi(a, b, c) {
+  return a * b * c;
+}
+
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn(...args);
+    } else {
+      return function (...nextArgs) {
+        return curried(...args, ...nextArgs);
+      };
+    }
+  };
+}
+
+const curriedSum = curry(multi);
+
+console.log(curriedSum(2)(2)(3));
